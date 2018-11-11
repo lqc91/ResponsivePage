2a06f9c278d6f5e492591f76e0a9ef21965a2496
lufan <ericlufan@163.com>
2018-11-11 23:41:36 +0800

    adapt to iPhone6/7/8

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
+// 移动端适配，REM布局
 (function(doc, win) {
   var docEl = doc.documentElement,
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
     recalc = function() {
-      var clientWidth = docEl.clientWidth;
-      console.log(clientWidth);
+      var clientWidth = docEl.clientWidth;  
       if (!clientWidth) return;
       if (clientWidth >= 980) {
         docEl.style.fontSize = '100px';
@@ -17,3 +17,22 @@
   doc.addEventListener('DOMContentLoaded', recalc, false);
   recalc();
 })(document, window);
+
+// 获取相关元素
+var barBtn = document.getElementById('barBtn'),
+  nav = document.getElementById('nav'),
+  showNav = false;
+
+// iPhone6下，顶部右侧方块按钮点击事件
+barBtn.addEventListener('click', function(){
+  nav.style.transition = 'top 1s';
+  // 若导航条已滑下，点击按钮，导航缓缓滑上去
+  if(showNav){
+    nav.style.top = '-1.6rem';
+    showNav = false;
+  }else{
+    // 若导航条未滑下，点击按钮，导航缓缓滑下
+    nav.style.top = '1rem';
+    showNav = true;
+  }
+});
